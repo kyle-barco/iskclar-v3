c447ccc45fd8147f62bb2edcfc9c3a1824bc87a7
Anzune <anzu.formento@gmail.com>
2026-06-22 00:22:44 +0800

    lastname validatio

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -32,7 +32,7 @@ const registerSchema = Joi.object({
                      .required(),
 
   // Name
-  last_name:       Joi.string().pattern(/^[A-Za-zÀ-ÿ\s'\-]{1,60}$/).required(),
+  last_name:       Joi.string().pattern(/^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ\s'-]{0,59}$/).required(),
   first_name:      Joi.string().pattern(/^[A-Za-zÀ-ÿ\s'\-]{1,60}$/).required(),
   middle_name:     Joi.string().pattern(/^[A-Za-zÀ-ÿ\s'\-]{1,60}$/).optional().allow(''),
   suffix:          Joi.string().valid('Jr.', 'Sr.', 'II', 'III', 'IV', 'V').optional().allow(''),
